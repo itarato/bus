9f4c05b72ddf5d760f2b213bda080c7a576bf10c
Peter Arato <it.arato@gmail.com>
2026-03-26 21:25:21 -0400

    Timeouyt.

--- src/bus.rs
+++ src/bus.rs
@@ -1,48 +1,67 @@
 use std::{
     collections::{HashMap, VecDeque},
-    sync::{Arc, Condvar, Mutex},
+    sync::{Arc, Condvar, Mutex, atomic::AtomicBool},
     thread::{JoinHandle, spawn},
+    time::{Duration, SystemTime},
 };
 
 use crate::message::Message;
 
 pub struct Queue {
-    queue_and_condvar: Arc<(Mutex<VecDeque<Message>>, Condvar)>,
+    queue: Arc<Mutex<VecDeque<Message>>>,
+    condvar: Arc<Condvar>,
 }
 
 impl Queue {
     fn new() -> Self {
         Self {
-            queue_and_condvar: Arc::new((Mutex::new(VecDeque::new()), Condvar::new())),
+            queue: Arc::new(Mutex::new(VecDeque::new())),
+            condvar: Arc::new(Condvar::new()),
         }
     }
 
     pub fn put(&self, message: Message) {
-        let (queue, cv) = &*self.queue_and_condvar;
-        let mut queue = queue.lock().unwrap();
-
+        let mut queue = self.queue.lock().unwrap();
         queue.push_back(message);
-
-        cv.notify_one();
+        self.condvar.notify_one();
     }
 
     pub fn get(&self) -> Option<Message> {
-        let (queue, _cv) = &*self.queue_and_condvar;
-        let mut queue = queue.lock().unwrap();
+        let mut queue = self.queue.lock().unwrap();
         queue.pop_front()
     }
 
     pub fn get_blocking(&self) -> Message {
-        let (queue, cv) = &*self.queue_and_condvar;
-
-        let mut queue = queue.lock().unwrap();
+        let mut queue = self.queue.lock().unwrap();
 
         loop {
             if let Some(message) = queue.pop_front() {
                 return message;
             }
 
-            queue = cv.wait(queue).unwrap();
+            queue = self.condvar.wait(queue).unwrap();
+        }
+    }
+
+    pub fn get_blocking_timeout(&self, mut duration: Duration) -> Option<Message> {
+        let mut queue = self.queue.lock().unwrap();
+        let deadline = SystemTime::now().checked_add(duration).unwrap();
+
+        loop {
+            if let Some(message) = queue.pop_front() {
+                return Some(message);
+            }
+
+            duration = match deadline.duration_since(SystemTime::now()) {
+                Ok(d) => d,
+                Err(_) => return None,
+            };
+            let (new_queue, wait_res) = self.condvar.wait_timeout(queue, duration).unwrap();
+            if wait_res.timed_out() {
+                return None;
+            }
+
+            queue = new_queue;
         }
     }
 }
@@ -75,24 +94,28 @@ pub struct Bus {
     incoming: Arc<Queue>,
     outgoing: Arc<Mutex<HashMap<String, Arc<Queue>>>>,
     thread_handle: JoinHandle<()>,
+    is_terminated: Arc<AtomicBool>,
 }
 
 impl Bus {
     pub fn new() -> Self {
         let incoming = Arc::new(Queue::new());
         let outgoing = Arc::new(Mutex::new(HashMap::new()));
+        let is_terminated = Arc::new(AtomicBool::new(false));
 
         let thread_handle = spawn({
             let incoming = incoming.clone();
             let outgoing = outgoing.clone();
+            let is_terminated = is_terminated.clone();
 
-            move || Self::work_thread(incoming, outgoing)
+            move || Self::work_thread(incoming, outgoing, is_terminated)
         });
 
         Self {
             incoming,
             outgoing,
             thread_handle,
+            is_terminated,
         }
     }
 
@@ -109,14 +132,27 @@ impl Bus {
         OutQueue { inner: queue }
     }
 
-    fn work_thread(incoming: Arc<Queue>, outgoing: Arc<Mutex<HashMap<String, Arc<Queue>>>>) {
+    fn work_thread(
+        incoming: Arc<Queue>,
+        outgoing: Arc<Mutex<HashMap<String, Arc<Queue>>>>,
+        is_terminated: Arc<AtomicBool>,
+    ) {
         loop {
-            let message = incoming.get_blocking();
-            let outgoing_guard = outgoing.lock().unwrap();
-            for (_, out_queue) in &*outgoing_guard {
-                out_queue.put(message.clone());
+            if let Some(message) = incoming.get_blocking_timeout(Duration::from_millis(10)) {
+                let outgoing_guard = outgoing.lock().unwrap();
+                for (_, out_queue) in &*outgoing_guard {
+                    out_queue.put(message.clone());
+                }
+            } else if is_terminated.load(std::sync::atomic::Ordering::Relaxed) {
+                break;
             }
         }
+    }
+
+    pub fn terminate(self) {
+        self.is_terminated
+            .store(true, std::sync::atomic::Ordering::Relaxed);
+        self.thread_handle.join().unwrap();
     }
 }
 
@@ -148,6 +184,8 @@ mod test {
 
         assert_eq!(listener_a.get(), None);
         assert_eq!(listener_b.get(), None);
+
+        bus.terminate();
     }
 
     fn make_message() -> Message {
